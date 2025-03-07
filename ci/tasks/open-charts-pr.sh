795671fe06e49b5562ed131fd6a6b0db916acd70
Benjamin Hindman <bhindman@proton.me>
2025-03-07 09:41:55 -0600

    ci: update pipeline

--- ci/tasks/open-charts-pr.sh
+++ ci/tasks/open-charts-pr.sh
@@ -8,34 +8,42 @@ export history_migrate_digest=$(cat ./history-migrate-edge-image/digest)
 
 pushd charts-repo
 
-ref=$(yq e '.image.git_ref' charts/galoy/charts/price/values.yaml)
+ref=$(yq e '.image.git_ref' charts/price/values.yaml)
 git checkout ${BRANCH}
-old_ref=$(yq e '.image.git_ref' charts/galoy/charts/price/values.yaml)
+old_ref=$(yq e '.image.git_ref' charts/price/values.yaml)
 
 cat <<EOF >> ../body.md
-# Bump galoy price images
+# Bump flash price images
 
-The galoy price image will be bumped to digest:
+The flash price image will be bumped to digest:
 \`\`\`
 ${digest}
 \`\`\`
 
-The galoy price-history image will be bumped to digest:
+The flash price-history image will be bumped to digest:
 \`\`\`
 ${history_digest}
 \`\`\`
 
-The galoy price-history-migrate image will be bumped to digest:
+The flash price-history-migrate image will be bumped to digest:
 \`\`\`
 ${history_migrate_digest}
 \`\`\`
 
 Code diff contained in this image:
 
-https://github.com/GaloyMoney/price/compare/${old_ref}...${ref}
+https://github.com/lnflash/price/compare/${old_ref}...${ref}
 EOF
 
-export GH_TOKEN="$(ghtoken generate -b "${GH_APP_PRIVATE_KEY}" -i "${GH_APP_ID}" | jq -r '.token')"
+# export GH_TOKEN="$(ghtoken generate -b "${GH_APP_PRIVATE_KEY}" -i "${GH_APP_ID}" | jq -r '.token')"
+export GH_TOKEN=${AUTH_TOKEN}
+gh auth setup-git
+
+gh repo set-default ${GH_ORG}/charts
+git config remote.origin.fetch "+refs/heads/*:refs/remotes/origin/*"
+
+export ref=$(cat ./repo/.git/short_ref)
+git checkout ${ref}
 
 gh pr close ${BOT_BRANCH} || true
 gh pr create \
@@ -43,5 +51,5 @@ gh pr create \
   --body-file ../body.md \
   --base ${BRANCH} \
   --head ${BOT_BRANCH} \
-  --label galoybot \
+  --label flashbot \
   --label price
