2858f4ed4177745f861888dfabe1dee31396f08a
brh28 <31115595+brh28@users.noreply.github.com>
2025-03-08 12:14:19 -0600

    ci: set pipeline in concourse (#5)

--- ci/tasks/bump-image-digest.sh
+++ ci/tasks/bump-image-digest.sh
@@ -9,13 +9,13 @@ export ref=$(cat ./repo/.git/short_ref)
 
 pushd charts-repo
 
-yq -i e '.realtime.image.digest = strenv(digest)' ./charts/galoy/charts/price/values.yaml
-yq -i e '.realtime.image.git_ref = strenv(ref)' ./charts/galoy/charts/price/values.yaml
-yq -i e '.history.image.digest = strenv(history_digest)' ./charts/galoy/charts/price/values.yaml
-yq -i e '.history.migrateImage.digest = strenv(history_migrate_digest)' ./charts/galoy/charts/price/values.yaml
+yq -i e '.realtime.image.digest = strenv(digest)' ./charts/price/values.yaml
+yq -i e '.realtime.image.git_ref = strenv(ref)' ./charts/price/values.yaml
+yq -i e '.history.image.digest = strenv(history_digest)' ./charts/price/values.yaml
+yq -i e '.history.migrateImage.digest = strenv(history_migrate_digest)' ./charts/price/values.yaml
 
 if [[ -z $(git config --global user.email) ]]; then
-  git config --global user.email "bot@galoy.io"
+  git config --global user.email "bot@flash.io"
 fi
 if [[ -z $(git config --global user.name) ]]; then
   git config --global user.name "CI Bot"
@@ -26,5 +26,5 @@ fi
   git merge --no-edit ${BRANCH}
   git add -A
   git status
-  git commit -m "chore(deps): bump galoy price image to '${digest}'"
+  git commit -m "chore(deps): bump flash price image to '${digest}'"
 )
